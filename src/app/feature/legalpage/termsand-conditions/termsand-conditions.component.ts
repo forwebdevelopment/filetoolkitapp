b57928a21f5505d9e37d9084f03c798be1d6cce7
AU COMPUTER'S <forwebdevelopmentuse@gmail.com>
2025-11-30 14:52:59 +0530

    updated code

---
+++ src/app/feature/legalpage/termsand-conditions/termsand-conditions.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-termsand-conditions',
+  imports: [],
+  templateUrl: './termsand-conditions.component.html',
+  styleUrl: './termsand-conditions.component.css'
+})
+export class TermsandConditionsComponent {
+
+}
